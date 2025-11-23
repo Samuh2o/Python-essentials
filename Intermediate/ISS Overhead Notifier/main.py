1a8229355275c5d0a019bd2d7ec1bbe6eb6120ec
samuel_hermsdorff <105018861+Samuel-Hermsdorff@users.noreply.github.com>
2025-11-23 04:24:19 -0300

    Add files via upload

---
+++ Intermediate/ISS Overhead Notifier/main.py
@@ -0,0 +1,29 @@
+import requests
+from datetime import datetime
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json", timeout=10)
+# response.raise_for_status()
+
+# data = response.json()
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+
+# iss_position = (longitude, latitude)
+# print(iss_position)
+
+MY_LAT = -23.694450
+MY_LONG = -46.565800
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+response = requests.get("https://api.sunrise-sunset.org/json", timeout=10, params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now().hour
